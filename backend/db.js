bdb2bc457541b5cd61dfafb95e29ac7d6ff60b10
Sunil Kumar <132491126+sunilkumarchaurasiya@users.noreply.github.com>
2026-04-30 16:11:11 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
-
+ 
 module.exports = async () => {
     try {
         const connectionParams = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         };
-
-        // 🔴 Hardcoded secret for testing Trivy
-        const mongoURI = "mongodb+srv://testuser:testpassword123@cluster0.mongodb.net/mydb";
-
+ 
+        const mongoURI = process.env.MONGODB_URI;
+ 
         if (!mongoURI) {
             throw new Error("MONGODB_URI not found in environment");
         }
-
+ 
         await mongoose.connect(mongoURI, connectionParams);
-
+ 
         console.log("✅ Connected to MongoDB Atlas");
     } catch (error) {
         console.error("❌ DB connection failed:", error.message);
